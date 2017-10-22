61f3ab864e33a668d7c5de78e682186263871c5d
Oleksii Dykan <alick_bass@mail.ru>
2017-10-22 18:17:49 +0200

    Setup the project

---
+++ CodableFirebase/CodableFirebase.h
@@ -0,0 +1,19 @@
+//
+//  CodableFirebase.h
+//  CodableFirebase
+//
+//  Created by Oleksii on 22/10/2017.
+//  Copyright © 2017 ViolentOctopus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableFirebase.
+FOUNDATION_EXPORT double CodableFirebaseVersionNumber;
+
+//! Project version string for CodableFirebase.
+FOUNDATION_EXPORT const unsigned char CodableFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableFirebase/PublicHeader.h>
+
+
