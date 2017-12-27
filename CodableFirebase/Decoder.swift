27d639724429409ac4bf0d7f9edfa05b062f1248
Oleksii Dykan <alick_bass@mail.ru>
2017-12-27 15:18:23 +0100

    Add firebase decoder

--- CodableFirebase/Decoder.swift
+++ CodableFirebase/Decoder.swift
@@ -9,44 +9,10 @@
 import Foundation
 
 class _FirebaseDecoder : Decoder {
-    /// The strategy to use for decoding `Date` values.
-    enum DateDecodingStrategy {
-        /// Defer to `Date` for decoding. This is the default strategy.
-        case deferredToDate
-        
-        /// Decode the `Date` as a UNIX timestamp from a JSON number.
-        case secondsSince1970
-        
-        /// Decode the `Date` as UNIX millisecond timestamp from a JSON number.
-        case millisecondsSince1970
-        
-        /// Decode the `Date` as an ISO-8601-formatted string (in RFC 3339 format).
-        @available(OSX 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)
-        case iso8601
-        
-        /// Decode the `Date` as a string parsed by the given formatter.
-        case formatted(DateFormatter)
-        
-        /// Decode the `Date` as a custom value decoded by the given closure.
-        case custom((_ decoder: Decoder) throws -> Date)
-    }
-    
-    /// The strategy to use for decoding `Data` values.
-    enum DataDecodingStrategy {
-        /// Defer to `Data` for decoding.
-        case deferredToData
-        
-        /// Decode the `Data` from a Base64-encoded string. This is the default strategy.
-        case base64
-        
-        /// Decode the `Data` as a custom value decoded by the given closure.
-        case custom((_ decoder: Decoder) throws -> Data)
-    }
-    
     /// Options set on the top-level encoder to pass down the decoding hierarchy.
     struct _Options {
-        let dateDecodingStrategy: DateDecodingStrategy?
-        let dataDecodingStrategy: DataDecodingStrategy?
+        let dateDecodingStrategy: FirebaseDecoder.DateDecodingStrategy?
+        let dataDecodingStrategy: FirebaseDecoder.DataDecodingStrategy?
         let userInfo: [CodingUserInfoKey : Any]
     }
     
